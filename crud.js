446632f222eb456497927fcdf8195e2826ccbefb
Alvarotloc <alvarotloc@gmail.com>
2022-06-15 23:23:15 +0200

    Parte backend del proyecto lista, subiendo a github

---
+++ crud.js
@@ -0,0 +1,108 @@
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const urlMongo = process.env.MONGO_CONECTION;
+const objConfig = {
+  useUnifiedTopology: true,
+};
+
+const conectar = () => MongoClient.connect(urlMongo, objConfig);
+
+const leerEventos = async () => {
+  const conexion = await conectar();
+  const db = conexion.db("eTournament");
+
+  return new Promise((callback) => {
+    db.collection("eTournament")
+      .find({})
+      .toArray((error, resultado) => {
+        conexion.close();
+        if (error) return callback(msgError);
+        return callback(resultado);
+      });
+  });
+};
+const comprobarExistenEventos = async (fecha) => {
+  const conexion = await conectar();
+  const db = conexion.db("eTournament");
+  return new Promise((callback) => {
+    db.collection("eTournament")
+      .find({ fecha })
+      .toArray((error, resultado) => {
+        conexion.close();
+        return callback(resultado);
+      });
+  });
+};
+const crearEvento = async (objEvento) => {
+  const conexion = await conectar();
+  const db = conexion.db("eTournament");
+  const { fecha } = objEvento;
+  const eventosExistentes = await comprobarExistenEventos(fecha);
+  if (eventosExistentes.length > 0) {
+    return { error: "Solo puede haber un evento" };
+  }
+  if (Object.keys(objEvento).length > 0) {
+    return new Promise((callback) => {
+      db.collection("eTournament").insertOne(objEvento, (error, resultado) => {
+        conexion.close();
+        if (error) return callback(error);
+        return callback(objEvento);
+      });
+    });
+  }
+};
+
+const borrarEvento = async (_id) => {
+  const conexion = await conectar();
+  const db = conexion.db("eTournament");
+
+  return new Promise((callback) => {
+    db.collection("eTournament").deleteOne(
+      { _id: ObjectId(_id) },
+      (error, resultado) => {
+        conexion.close();
+        if (error) return callback(msgError);
+        return callback({ _id });
+      }
+    );
+  });
+};
+
+const actualizarEvento = async (objEvento) => {
+  const conexion = await conectar();
+  const db = conexion.db("eTournament");
+  const { _id, fecha, descripcion, participantes, nombre } = objEvento;
+
+  let resultado = await db
+    .collection("eTournament")
+    .find({ _id: ObjectId(_id) })
+    .toArray();
+
+
+  if (resultado.length > 0) {
+    const eventosExistentes = await comprobarExistenEventos(fecha);
+    if(resultado[0].fecha === fecha){
+      return new Promise((callback) => {
+        db.collection("eTournament").updateOne(
+          { _id: ObjectId(_id) },
+          { $set: { fecha, descripcion, participantes, nombre } },
+          (error, respuesta) => {
+            conexion.close();
+            if (error) {
+              callback(error);
+            } else {
+              callback(objEvento);
+            }
+          }
+        );
+      });
+    }else if(eventosExistentes.length > 0) {
+      return { error: "Solo puede haber un evento" };
+    }
+
+  }
+};
+
+export { leerEventos, crearEvento, borrarEvento, actualizarEvento };
---
+++ index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import {
+    leerEventos,
+    crearEvento,
+    borrarEvento,
+    actualizarEvento,
+} from "./crud.js";
+
+dotenv.config();
+
+const servidor = express();
+
+servidor.use(express.json());
+
+const whiteList = [ //Creamos la lista blanca de urls que vamos a admitir desde el backend
+  process.env.FRONTEND_URL,
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.includes(origin)) { //Si la url de la request está en la lista blanca, permitimos la request
+      callback(null, true);
+    } else {
+      callback(new Error("Error de Cors"));
+    }
+  },
+};
+
+
+servidor.use(cors(corsOptions));
+
+servidor
+  .route("/eventos")
+  .get(async (req, res) => {
+    let eventos = await leerEventos();
+    res.send(eventos);
+  })
+  .post(async (req, res) => {
+    let eventoCreado = await crearEvento(req.body);
+    res.send(eventoCreado);
+  })
+  .delete(async (req, res) => {
+    let idBorrado = await borrarEvento(req.body._id);
+    res.send(idBorrado);
+  })
+  .put(async (req, res) => {
+    let eventoActualizado = await actualizarEvento(req.body);
+    res.send(eventoActualizado);
+  });
+
+const port = process.env.PORT || 4000;
+
+servidor.listen(port, () => {
+  console.log(`Servidor en puerto ${port}`);
+});
